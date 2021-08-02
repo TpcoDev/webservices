856e67e2f8c98918e1f1ba4c1db23c166ab3bc32
wmoreno <walter.moreno@tpco.cl>
2021-08-02 13:55:55 -0400

    correccion paquete

--- helpdesk_rest/__manifest__.py
+++ helpdesk_rest/__manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Helpdesk',
-    'version': '12.0.0.1',
+    'version': '12.0.0.20210802',
     
     # any module necessary for this one to work correctly
     'depends': ['contacts', 'helpdesk', 'base_rest', "component"],
